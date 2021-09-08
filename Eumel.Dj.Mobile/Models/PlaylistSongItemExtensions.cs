73f30db857cd5bbbb1f6b81199a6fd09c11aa563
Thomas Ley <thomas-ley@outlook.de>
2021-09-08 11:38:34 +0200

    icons replaces with eumel logo.

---
+++ Eumel.Dj.Mobile/Models/PlaylistSongItemExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Eumel.Dj.Mobile.Services;
+
+namespace Eumel.Dj.Mobile.Models
+{
+    public static class PlaylistSongItemExtensions
+    {
+        public static PlaylistSongItem ToPlaylistSongItem(this VotedSong source, SongType songType, ISettingsService settings)
+        {
+            if (source == null) return null;
+
+            return new PlaylistSongItem()
+            {
+                Id = source.Id,
+                Title = source.Name,
+                Description = $"{source.Artist} - {source.Album}",
+                VoteCount = source.Voters.Count,
+                Type = songType,
+                VotedByMe = source.Voters.Any(x =>
+                    string.Compare(x, settings.Username, StringComparison.CurrentCultureIgnoreCase) == 0)
+            };
+        }
+    }
+}
---
+++ Eumel.Dj.Ui/Services/LoggingService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Eumel.Dj.WebServer.Logging;
+using Eumel.Dj.WebServer.Messages;
+using Microsoft.Extensions.Logging;
+using TinyMessenger;
+
+namespace Eumel.Dj.Ui.Services
+{
+    public class LoggingService : IDisposable
+    {
+        private readonly ITinyMessengerHub _hub;
+        private readonly IEumelLogger _logger;
+        private readonly List<TinyMessageSubscriptionToken> _tinyMessageSubscriptions;
+
+        public LoggingService(ITinyMessengerHub hub, IEumelLogger logger)
+        {
+            _hub = hub ?? throw new ArgumentNullException(nameof(hub));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _tinyMessageSubscriptions = new List<TinyMessageSubscriptionToken>(new[]
+            {
+                hub.Subscribe((Action<LogMessage>)Log),
+            });
+        }
+
+        private void Log(LogMessage message)
+        {
+            switch (message.Level)
+            {
+                case LogLevel.Trace:
+                    _logger.Verbose(message.Message);
+                    break;
+                case LogLevel.Debug:
+                    _logger.Debug(message.Message);
+                    break;
+                case LogLevel.Information:
+                    _logger.Information(message.Message);
+                    break;
+                case LogLevel.Warning:
+                    _logger.Warning(message.Message);
+                    break;
+                case LogLevel.Error:
+                    _logger.Error(message.Message, message.Exception);
+                    break;
+                case LogLevel.Critical:
+                    _logger.Fatal(message.Message, message.Exception);
+                    break;
+                case LogLevel.None:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            message.Response ??= new MessageResponse();
+        }
+
+        public void Dispose()
+        {
+            _tinyMessageSubscriptions.ForEach(x => _hub.Unsubscribe(x));
+        }
+    }
+}
