d4997a2387542ff0a6099ab0de537d2ab730eb29
Thomas Ley <thomas-ley@outlook.de>
2021-10-03 16:24:26 +0200

    documentation and publishing to google play store

--- Eumel.Dj.Mobile/Services/SettingsService.cs
+++ Eumel.Dj.Mobile/Services/SettingsService.cs
@@ -15,7 +15,9 @@ namespace Eumel.Dj.Mobile.Services
         {
             Username = Preferences.Get(SettingPrefix + nameof(Username), Marvel.Names.Random());
             RestEndpoint = Preferences.Get(SettingPrefix + nameof(RestEndpoint), null);
+#if DEBUG
             SyslogServer = Preferences.Get(SettingPrefix + nameof(SyslogServer), null);
+#endif
             Token = Preferences.Get(SettingPrefix + nameof(Token), null);
             MinimumLogLevel = (EumelLogLevel)Enum.Parse(typeof(EumelLogLevel), Preferences.Get(SettingPrefix + nameof(MinimumLogLevel), EumelLogLevel.Warn.ToString()));
 
@@ -28,7 +30,9 @@ namespace Eumel.Dj.Mobile.Services
             DependencyService.Get<ISyslogService>().Debug("Settings changing");
             RestEndpoint = restEndpoint;
             Username = username;
+#if DEBUG
             SyslogServer = syslogServer;
+#endif
             Token = token;
             MinimumLogLevel = minimumLogLevel;
 
@@ -41,7 +45,9 @@ namespace Eumel.Dj.Mobile.Services
             Preferences.Set(SettingPrefix + nameof(RestEndpoint), RestEndpoint);
             Preferences.Set(SettingPrefix + nameof(Username), Username);
             Preferences.Set(SettingPrefix + nameof(Token), Token);
+#if DEBUG
             Preferences.Set(SettingPrefix + nameof(SyslogServer), SyslogServer);
+#endif
             Preferences.Set(SettingPrefix + nameof(MinimumLogLevel), MinimumLogLevel.ToString());
         }
 
@@ -73,7 +79,12 @@ namespace Eumel.Dj.Mobile.Services
         public string RestEndpoint { get; private set; }
         public string Username { get; private set; }
         public string Token { get; private set; }
+#if DEBUG
         public string SyslogServer { get; private set; }
+#endif
+#if RELEASE
+        public string SyslogServer => null;
+#endif
         public EumelLogLevel MinimumLogLevel { get; set; }
     }
 }
--- Eumel.Dj.Ui/App.xaml.cs
+++ Eumel.Dj.Ui/App.xaml.cs
@@ -1,7 +1,4 @@
 ﻿using System.Windows;
-using Eumel.Core.Logging;
-using Eumel.Dj.Ui.AutoStartServices;
-using Eumel.Logging.Serilog;
 using TinyMessenger;
 
 namespace Eumel.Dj.Ui
@@ -10,23 +7,11 @@ namespace Eumel.Dj.Ui
     {
         private TinyMessengerHub _hub;
 
-        // ReSharper disable once NotAccessedField.Local
-        private LoggingService _loggingService;
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            // todo: there is a class with system settings we needs to inject
-            var loggerSettings = new LoggerSettings
-            {
-                Filelog = new FilelogSettings { EnableFileLogging = false },
-                Syslog = new SyslogSettings { EnableSyslogLogging = true, SysLogServerIp = "192.168.178.37", UseUdp = true },
-            };
-            var logger = new SerilogFactory().Build(loggerSettings);
-
             _hub = TinyMessengerHub.DefaultHub;
-            _loggingService = new LoggingService(_hub, logger);
         }
     }
 }
